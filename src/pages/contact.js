eea170124d3bcd4e302af6fb7ed996dcda385a34
Sandesh Yadav <sandeshyadav@Sandeshs-MacBook-Pro.local>
2020-01-20 08:26:14 -0500

    contact page updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Head from '../components/head';
 
 import Layout from '../components/layout';
 
 const ContactPage = () => {
     return (
             <Layout>
+            <Head title="Contact"/>
             <h1>Contact Page</h1>
             <p> If you need me call me. You matter where you are, no matter how far.</p>
             <p>Linking to external site <a rel="noopener noreferrer" href="https://www.espn.com/soccer/?src=com" target="_blank">soccer</a></p>
